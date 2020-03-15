5d754453e22823980d80d2b9b0c982e8d00027b4
Alec <alecsky3@gmail.com>
2020-03-15 19:02:18 +1000

    update password checker functions

--- prac_03/password-check.py
+++ prac_03/password-check.py
@@ -2,20 +2,25 @@
 MIN_LENGTH = 2
 MAX_LENGTH = 6
 
-
 def main():
-    print("Please input a password that is between {} and {} characters long".format(MIN_LENGTH, MAX_LENGTH))
-    password = input(">>> ")
+
+    password = get_password()
     while not password_checker(password):
         print("Invalid password!")
         main()
-    print(string_length_to_character_converter(password, "*"))
+    print(string_length_to_character_converter(password))
 
 
-def string_length_to_character_converter(string_to_convert, replacement_character):
+def get_password():
+    print("Please input a password that is between {} and {} characters long".format(MIN_LENGTH, MAX_LENGTH))
+    password = input(">>> ")
+    return password
+
+
+def string_length_to_character_converter(string_to_convert):
     converted_string = ""
     for i in range(len(string_to_convert)):
-        converted_string += replacement_character
+        converted_string += "*"
     return converted_string
 
 
