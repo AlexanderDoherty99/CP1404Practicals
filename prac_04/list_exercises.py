708b8ec17767a7fa3352b8a3724bd751afb352e3
Alec <alecsky3@gmail.com>
2020-03-22 21:48:33 +1000

    add list of usernames

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -10,3 +10,5 @@ print("the last number is {}".format(numbers[-1]))
 print("the smallest number is {}".format(min(numbers)))
 print("the largest number is {}".format(max(numbers)))
 print("the average of the numbers is {:.1f}".format(sum(numbers)/len(numbers)))
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
