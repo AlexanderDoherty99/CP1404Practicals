854c1e08a19f635685a505009b5dc5a72931785f
Alec <alecsky3@gmail.com>
2020-03-22 21:31:03 +1000

    add draft of subject details print function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,13 +7,16 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    data_arrays = get_data()
+    print(data_arrays)
+    for data in range(len(data_arrays)):
+        print("{} is taught be {} and has {} students".format(data_arrays[data][0], data_arrays[data][1], data_arrays[data][2]))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data_arrays = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +25,10 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data_arrays.append(parts)
         print("----------")
     input_file.close()
+    return data_arrays
 
 
 main()
