deab1510e892d28f05f993c973086c5baaeb7ba1
Alec <alecsky3@gmail.com>
2020-03-22 21:43:12 +1000

    add print for statistics (pre-test)

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -5,3 +5,8 @@ for i in range(NUMBERS_WANTED):
     numbers.append(input("Number: "))
 print(numbers)
 
+print("the first number is {}".format(numbers[0]))
+print("the last number is {}".format(numbers[-1]))
+print("the smallest number is {}".format(min(numbers)))
+print("the largest number is {}".format(max(numbers)))
+print("the average of the numbers is {}".format(sum(numbers)//len(numbers)))
