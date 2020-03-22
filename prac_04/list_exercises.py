ef58f41f64f02da508dd38f1540bef3031a80d1c
Alec <alecsky3@gmail.com>
2020-03-22 21:50:30 +1000

    add username checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,5 +1,13 @@
 NUMBERS_WANTED = 5
 numbers = []
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username_is_valid = False
+while not username_is_valid:
+    input_username = input("Username: ")
+    if input_username in usernames:
+        username_is_valid = True
+
 
 for i in range(NUMBERS_WANTED):
     numbers.append(int(input("Number: ")))
@@ -11,4 +19,3 @@ print("the smallest number is {}".format(min(numbers)))
 print("the largest number is {}".format(max(numbers)))
 print("the average of the numbers is {:.1f}".format(sum(numbers)/len(numbers)))
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
