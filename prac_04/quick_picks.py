d5a661e8dd2d39da4166f206639cd69267586c85
Alec <alecsky3@gmail.com>
2020-03-22 22:45:02 +1000

    drafted an attempt at fixing third attempt after realizing error!

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,8 +8,15 @@ quick_picks = []
 for quick_pick in range(desired_number_of_picks):
     # current_pick_line = [random.randint(1, 10) for number in range(6) if number not in ????]
     current_pick_line = []
+    random_test = []
+    print([random.randint(1, 10) for number in range(6) if number not in random_test])
+    current_pick_line = [random.randint(1, 10) for number in range(6) if number not in current_pick_line]
     # i = 0
-    current_pick_line = [random.randint(1, 45) for number in range(6) if number not in current_pick_line]
+    # for number in range(6):
+    #     random_number = random.randint(1,10)
+    #     if random_number not in current_pick_line:
+    #         current_pick_line.append(random_number)
+    #         break
     # while i < 6:
     #     random_number = random.randint(LOWEST_NUMBER, HIGHEST_NUMBER)
     #     if random_number not in current_pick_line:
