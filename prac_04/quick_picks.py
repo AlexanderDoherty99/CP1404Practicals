e7b170353e2c8849445900fa726df1c3fee2922a
Alec <alecsky3@gmail.com>
2020-03-22 22:18:28 +1000

    add and tested method of ensuring numbers don't repeat (failed)

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,6 +1,7 @@
 import random
 
-desired_number_of_picks = input("How many quick picks? ")
+desired_number_of_picks = int(input("How many quick picks? "))
 quick_picks = []
-for quick_pick in range(desired_number_of_picks+1):
+for quick_pick in range(desired_number_of_picks + 1):
+    # current_pick_line = [random.randint(1, 10) for number in range(6) if number not in ????]
 
