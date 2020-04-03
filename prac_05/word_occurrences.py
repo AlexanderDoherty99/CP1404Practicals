cf7287a8ad10c1c75751309120de39c8d67ab909
Alec <alecsky3@gmail.com>
2020-04-03 11:10:04 +1000

    add code for iterating and printing dictionary keys & values

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,12 +7,13 @@ def create_wordcount_dictionary_from_text(text):
             words_count_dict[word] += 1
     return words_count_dict
 
+
 def main():
 
     user_input = input("Please input a string: ").lower()
     words_count_dict = create_wordcount_dictionary_from_text(user_input)
-    print(words_count_dict)
-
+    for key in words_count_dict.keys():
+        print("{} : {}".format(key, words_count_dict[key]))
 
 
 main()
