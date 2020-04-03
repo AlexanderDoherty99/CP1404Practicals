73bcb9731803e2a747db96ee87d17345bca17f21
Alec <alecsky3@gmail.com>
2020-04-03 16:08:34 +1000

    make corrections to code

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,10 +6,11 @@ def main():
         email = input("Email:")
         username = email.split("@")[0]
         user_name = username.replace(".", " ").title()
-        correct_name_checker = input("Is your name {}? (Y/n)".format(user_name))
-        if correct_name_checker[0].upper() == "N"
-            user_name = input("Name: ")
-        emails_dict[user_name] = email
+        if not user_name == "":
+            correct_name_checker = input("Is your name {}? (Y/n)".format(user_name))
+            if correct_name_checker[0].upper() == "N":
+                user_name = input("Name: ")
+            emails_dict[user_name] = email
     for key in emails_dict:
         print("{} ({})".format(key, emails_dict[key]))
 
