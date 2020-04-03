1e3dd83ad0f0f491be9d5d61ee324b3fd2bbb141
Alec <alecsky3@gmail.com>
2020-04-03 10:32:10 +1000

    complete formatting in state_names and add hex_colours

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,14 +4,28 @@ State names in a dictionary
 File needs reformatting
 """
 
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
+def output_formatted_dictionary(dictionary):
+    for item in dictionary:
+        print("{:<3} is {}".format(item, dictionary[item]))
+
+
+def main():
+
+    CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                    "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+    print(CODE_TO_NAME)
+
     state_code = input("Enter short state: ").upper()
+    while state_code != "":
+        if state_code in CODE_TO_NAME:
+            print(state_code, "is", CODE_TO_NAME[state_code])
+        else:
+            print("Invalid short state")
+        state_code = input("Enter short state: ").upper()
+
+    output_formatted_dictionary(CODE_TO_NAME)
+
+
+main()
+
