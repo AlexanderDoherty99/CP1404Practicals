dcc2fe7ebd02d1623fbf4aa73b263eabd7b821ef
Alec <alecsky3@gmail.com>
2020-04-03 11:02:22 +1000

    move dictionary creation into separate function

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,12 +1,16 @@
-def main():
-
-    user_input = input("Please input a string: ").lower()
+def create_wordcount_dictionary_from_text(text):
     words_count_dict = {}
-    for word in user_input.split():
+    for word in text.split():
         if word not in words_count_dict:
             words_count_dict[word] = 1
         else:
             words_count_dict[word] += 1
+    return words_count_dict
+
+def main():
+
+    user_input = input("Please input a string: ").lower()
+    words_count_dict = create_wordcount_dictionary_from_text(user_input)
     print(words_count_dict)
 
 
