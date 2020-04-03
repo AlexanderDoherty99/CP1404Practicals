09dc1708801200840e5208cac0d17c9633a26128
Alec <alecsky3@gmail.com>
2020-04-03 15:58:58 +1000

    add name creator

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,7 +4,8 @@ def main():
     user_input = "temp"
     while not user_input == "":
         user_input = input("Email:")
-        user_name = user_input.split("@")[0]
+        username = user_input.split("@")[0]
+        user_name = username.replace(".", " ").title()
         print("Is your name {}?".format(user_name))
 
 main()
