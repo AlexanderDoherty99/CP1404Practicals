18c237181d45bcffc46701a3530a1117a7a95357
Alec <alecsky3@gmail.com>
2020-04-19 21:23:31 +1000

    update printing loop with enumerate function

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -16,8 +16,7 @@ def main():
     #         user_finished = True
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-    i = 0
-    for guitar in guitars:
+    for i, guitar in enumerate(guitars, 0):
         vintage_string = " (vintage)" if guitar.is_vintage() >= True else ""
         print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost, vintage_string))
         i += 1
