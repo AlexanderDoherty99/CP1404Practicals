7044237f85d1c49fb500bc7b0b6d67dc6bc9820d
Alec <alecsky3@gmail.com>
2020-04-19 20:44:32 +1000

    correct type and add dyanamic list printing function

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -7,7 +7,16 @@ def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    langauges = [ruby, python, visual_basic]
+    languages = [ruby, python, visual_basic]
+
+    list_dynamic(languages)
+
+
+def list_dynamic(languages):
+    print("The dynamically typed languages are:")
+    for row in languages:
+        if row.is_dynamic():
+            print(row.name)
 
 
 main()
