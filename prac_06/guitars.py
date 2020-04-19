cd7f5f311a25e17ac5275bce039886ce82e2b950
Alec <alecsky3@gmail.com>
2020-04-19 21:27:04 +1000

    update string printing with cool new method

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -18,7 +18,9 @@ def main():
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     for i, guitar in enumerate(guitars, 0):
         vintage_string = " (vintage)" if guitar.is_vintage() >= True else ""
-        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost, vintage_string))
+        print("Guitar {0}: {current_guitar.name:>20} "
+              "({current_guitar.year}), "
+              "worth ${current_guitar.cost:10,.2f}{1}".format(i + 1, vintage_string, current_guitar=guitar,))
         i += 1
 
 main()
