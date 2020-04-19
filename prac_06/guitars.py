5d5afb88b6a7ddb8b925bb7c5e8366a5eadcc881
Alec <alecsky3@gmail.com>
2020-04-19 21:16:17 +1000

    update commented user input loop to reflect adding of data to object

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -10,11 +10,12 @@ def main():
     #     current_guitar.append(str(input("Name:")))
     #     current_guitar.append(int(input("Year: ")))
     #     current_guitar.append(float(input("Cost:")))
-    #     guitars.append(current_guitar)
+    #     guitars.append(Guitar(current_guitar[0], current_guitar[1], current_guitar[2]))
     #     user_input = input("Add another Guitar? Y/n: ")
     #     if user_input[0].upper() == "N":
     #         user_finished = True
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    print(guitars)
 
 main()
