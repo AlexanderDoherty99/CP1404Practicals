3cd2c6f009700686e08f5fe75d000fe8c240d877
Alec <alecsky3@gmail.com>
2020-04-19 20:41:26 +1000

    test class printing and correct typo

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -7,7 +7,7 @@ class ProgrammingLanguage:
         self.year = year
 
     def __str__(self):
-        return "{}, {} Typing. Reflection={}, First appeared in {}".format(self.name,
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name,
                                                                            self.typing,
                                                                            self.reflection,
                                                                            self.year)
