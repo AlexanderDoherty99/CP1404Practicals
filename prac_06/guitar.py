ff8727d4a0024dc5d11b27d3a1b75e41bf490595
Alec <alecsky3@gmail.com>
2020-04-19 20:53:58 +1000

    update constructor to allow for input values

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,8 +1,11 @@
 class Guitar:
-    def __init__(self):
-        self.name = ""
-        self.year = 0
-        self.cost = 0
+    def __init__(self, name, year, cost):
+        if name == None:
+            self.name = ""
+        if year == None:
+            self.year = 0
+        if cost == None:
+            self.cost = 0
 
     def __str__(self):
         return "{} ({}) : ${}".format(self.name, self.year, self.cost)
