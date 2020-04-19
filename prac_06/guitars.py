4c0e8c34884913543421a41037296c5bc5edfea4
Alec <alecsky3@gmail.com>
2020-04-19 21:22:52 +1000

    add loop to print guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -16,6 +16,10 @@ def main():
     #         user_finished = True
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-    print(guitars)
+    i = 0
+    for guitar in guitars:
+        vintage_string = " (vintage)" if guitar.is_vintage() >= True else ""
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost, vintage_string))
+        i += 1
 
 main()
