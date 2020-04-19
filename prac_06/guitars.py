92d5dbeaf3f747dfc08f5ac46245c8a68a0a89fe
Alec <alecsky3@gmail.com>
2020-04-19 21:12:37 +1000

    add user input loop

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,1 +1,17 @@
 from prac_06.guitar import Guitar
+
+def main():
+
+    guitars = []
+    print("My guitars")
+    user_finished = False
+    while not user_finished:
+        current_guitar = []
+        current_guitar.append(str(input("Name:")))
+        current_guitar.append(int(input("Year: ")))
+        current_guitar.append(float(input("Cost:")))
+        guitars.append(current_guitar)
+        user_input = input("Add another Guitar? Y/n: ")
+        if user_input[0].upper() == "N":
+            user_finished = True
+main()
