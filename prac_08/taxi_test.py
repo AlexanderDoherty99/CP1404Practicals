012325ffe0a7a666874a20501232fe41280eec2b
Alec <alecsky3@gmail.com>
2020-05-16 12:57:52 +1000

    updated to reflect __str__

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -4,11 +4,9 @@ from prac_08.taxi import Taxi
 def main():
     taxi1 = Taxi("Prius", 100)
     taxi1.drive(40)
-    print("Name: {}, Fuel: {}, Fare: ${}, Price per KM: {}, Total KM: {},".format
-          (taxi1.name, taxi1.fuel, taxi1.get_fare(), taxi1.price_per_km, taxi1.current_fare_distance))
+    print(taxi1)
     taxi1.start_fare()
     taxi1.drive(100)
-    print("Name: {}, Fuel: {}, Fare: ${}, Price per KM: {}, Total KM: {},".format
-          (taxi1.name, taxi1.fuel, taxi1.get_fare(), taxi1.price_per_km, taxi1.current_fare_distance))
+    print(taxi1)
 
 main()
