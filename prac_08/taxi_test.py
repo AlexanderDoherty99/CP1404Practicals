362bde0ac7c5684ec119ba239636e4bc40201239
Alec <alecsky3@gmail.com>
2020-05-16 12:30:05 +1000

    update print statements to display information in more readable format

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -4,11 +4,11 @@ from prac_08.taxi import Taxi
 def main():
     taxi1 = Taxi("Prius", 100)
     taxi1.drive(40)
-    print("Name: {} Fuel: {} Fare: ${} Price per KM: {} Total KM: {}".format
+    print("Name: {}, Fuel: {}, Fare: ${}, Price per KM: {}, Total KM: {},".format
           (taxi1.name, taxi1.fuel, taxi1.get_fare(), taxi1.price_per_km, taxi1.current_fare_distance))
     taxi1.start_fare()
     taxi1.drive(100)
-    print("Name: {} Fuel: {} Fare: ${} Price per KM: {} Total KM: {}".format
+    print("Name: {}, Fuel: {}, Fare: ${}, Price per KM: {}, Total KM: {},".format
           (taxi1.name, taxi1.fuel, taxi1.get_fare(), taxi1.price_per_km, taxi1.current_fare_distance))
 
 
