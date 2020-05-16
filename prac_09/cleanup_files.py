2178b913da6d4729c84b5d954a526dfe6debbfb8
Alec <alecsky3@gmail.com>
2020-05-16 17:21:37 +1000

    remove unnecessary code

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -9,36 +9,16 @@ SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
 def main():
-    """Demo os module functions."""
-    print("Starting directory is: {}".format(os.getcwd()))
-
-    # Change to desired directory
-    os.chdir('Lyrics/Old')
-
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
-    # Make a new directory
-    # The next time you run this, it will crash if the directory exists
-    try:
-        os.mkdir('temp')
-    except FileExistsError:
-        pass
-
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
-        # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
-
-        # Option 2: move file to new place, with new name
-        # shutil.move(filename, 'temp/' + new_name)
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(directory_name + filename, new_name))
+            os.rename(os.path.join(directory_name, filename), os.path.join(directory_name, new_name))
 
 
 def get_fixed_filename(filename):
@@ -67,19 +47,5 @@ def get_fixed_filename(filename):
     return new_name
 
 
-def demo_walk():
-    os.chdir('Lyrics')
-    for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
-        # for filename in filenames:
-        #     new_name = get_fixed_filename(filename)
-        #     print("Renaming {} to {}".format(directory_name + filename, new_name))
-        #     os.rename(os.path.join(directory_name, filename), os.path.join(directory_name, new_name))
-
-
 main()
-# demo_walk()
 
