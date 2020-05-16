f545e2ce6f9aa06d5117ad38bf73060853d3aea9
Alec <alecsky3@gmail.com>
2020-05-16 13:06:34 +1000

    update print statement one more time

--- prac_08/SilverServiceTaxi_test.py
+++ prac_08/SilverServiceTaxi_test.py
@@ -2,9 +2,9 @@ from prac_08.SilverServiceTaxi import SilverServiceTaxi
 
 
 def main():
-    taxi2 = SilverServiceTaxi("Prius", 100, 2)
-    taxi2.drive(18)
+    taxi2 = SilverServiceTaxi("Hummer", 100, 2)
     print(taxi2)
+    taxi2.drive(18)
     print("Fare of ${}".format(taxi2.get_fare()))
 
 main()
