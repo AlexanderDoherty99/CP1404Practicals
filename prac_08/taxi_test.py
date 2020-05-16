60dfd0f9006f61d07c1f121eebd724f15cc82a8e
Alec <alecsky3@gmail.com>
2020-05-16 12:29:23 +1000

    update testing code to add more details

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -4,10 +4,12 @@ from prac_08.taxi import Taxi
 def main():
     taxi1 = Taxi("Prius", 100)
     taxi1.drive(40)
-    print("Name: {} Fuel: {} Fare: ${}".format(taxi1.name, taxi1.fuel, taxi1.get_fare()))
+    print("Name: {} Fuel: {} Fare: ${} Price per KM: {} Total KM: {}".format
+          (taxi1.name, taxi1.fuel, taxi1.get_fare(), taxi1.price_per_km, taxi1.current_fare_distance))
     taxi1.start_fare()
     taxi1.drive(100)
-    print("Name: {} Fuel: {} Fare: ${}".format(taxi1.name, taxi1.fuel, taxi1.get_fare()))
+    print("Name: {} Fuel: {} Fare: ${} Price per KM: {} Total KM: {}".format
+          (taxi1.name, taxi1.fuel, taxi1.get_fare(), taxi1.price_per_km, taxi1.current_fare_distance))
 
 
 main()
