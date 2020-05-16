c3046c5046345dd283989c0a18a5d4bcf125ff81
Alec <alecsky3@gmail.com>
2020-05-16 12:26:46 +1000

    update testing code to reflect class variable changes

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,11 +2,12 @@ from prac_08.taxi import Taxi
 
 
 def main():
-    taxi1 = Taxi("Prius", 100, 1.23)
+    taxi1 = Taxi("Prius", 100)
     taxi1.drive(40)
     print("Name: {} Fuel: {} Fare: ${}".format(taxi1.name, taxi1.fuel, taxi1.get_fare()))
     taxi1.start_fare()
     taxi1.drive(100)
     print("Name: {} Fuel: {} Fare: ${}".format(taxi1.name, taxi1.fuel, taxi1.get_fare()))
+
 
 main()
