e871b5c7b3467f956f52cf60860068ca941ce9d8
Alec <alecsky3@gmail.com>
2020-05-17 21:10:02 +1000

    add comments and improve dictionary with arrays as values to allow for multiple values per key.

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -9,19 +9,25 @@ def main():
         if os.path.isdir(filename):
             continue
         extension = filename[int(filename.index(".")):]
-        if extension not in categories:
+        # If the extension is not categorised, ask user to do so.
+        if not get_key(extension, categories) in categories.keys():
             desired_category = input("What category would you like to sort {} files into? ".format(extension[1:]))
-            categories[desired_category] = extension
-            os.mkdir(desired_category)
-        if ".doc" in categories.values():
-            print(get_key(extension, categories))
-        # shutil.move(filename, '{}/'.format(extension[1:]) + filename)
+            # Checks if category already exists in dictionary
+            if desired_category not in categories.keys():
+                categories[desired_category] = []
+            # Checks if Folder already exists for category
+            if not os.path.exists(desired_category):
+                os.mkdir(desired_category)
+            # Adds extension into category dictionary
+            categories[desired_category].append(extension)
+        # Find which category the extension belongs in and move it there
+        shutil.move(filename, '{}/'.format(get_key(extension,categories)) + filename)
 
 
 def get_key(check_value, dictionary):
+    # Check all value arrays for desired value, if found, return the parent key
     for key, value in dictionary.items():
-        if check_value == value:
+        if check_value in value:
             return key
-    return "no key found"
 
 main()
