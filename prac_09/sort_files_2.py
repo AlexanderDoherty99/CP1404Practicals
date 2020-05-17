7bb4146ed92ee95acfc9c162803235f9f3bc736e
Alec <alecsky3@gmail.com>
2020-05-17 20:36:45 +1000

    add dictionary and dictionary checker

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -4,14 +4,24 @@ import os
 
 def main():
     os.chdir('FilesToSort')
-    filetypes = []
+    categories = {}
     for filename in os.listdir('.'):
         if os.path.isdir(filename):
             continue
         extension = filename[int(filename.index(".")):]
-        if extension not in filetypes:
-            filetypes.append(extension)
-            os.mkdir(extension[1:])
-        shutil.move(filename, '{}/'.format(extension[1:]) + filename)
+        if extension not in categories:
+            desired_category = input("What category would you like to sort {} files into? ".format(extension[1:]))
+            categories[desired_category] = extension
+            os.mkdir(desired_category)
+        if ".doc" in categories.values():
+            print(get_key(extension, categories))
+        # shutil.move(filename, '{}/'.format(extension[1:]) + filename)
+
+
+def get_key(check_value, dictionary):
+    for key, value in dictionary.items():
+        if check_value == value:
+            return key
+    return "no key found"
 
 main()
