9b4d19e992209a523db25f274b2ace1b6fa8b469
Alec <alecsky3@gmail.com>
2020-05-17 14:44:26 +1000

    add code to add filetypes to array and move files

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -9,6 +9,10 @@ def main():
         if os.path.isdir(filename):
             continue
         extension = filename[int(filename.index(".")):]
+        if extension not in filetypes:
+            filetypes.append(extension)
+            os.mkdir(extension[1:])
+        shutil.move(filename, '{}/'.format(extension[1:]) + filename)
 
 
 main()
